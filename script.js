7e60d2f1cc5ead6f8f4ce70fa174d01aee011308
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-03 15:51:19 +0300

    a

---
+++ script.js
@@ -0,0 +1,34 @@
+1
+let user = prompt("зберігання температури");
+
+2
+let UserName = prompt("зберігання кількості днів у місяці");
+if (UserName === 31) {
+    alert("Дякую за підтвердження")
+}
+
+3
+let b = 100;
+ a = b - 1;
+ console.log(a);
+
+ 4
+ let c = 200;
+ y = 200 / 100 * 10;
+ console.log(y);
+
+ 5
+ console.log(Math.min(25, 2.5, 5));
+
+ 6
+ console.log(Number.parseFloat('1234567.99adfg0jhkk'));
+ 
+7
+console.log
+
+8
+let r = 10;
+console.log(Math.sqrt(10));
+
+
+
